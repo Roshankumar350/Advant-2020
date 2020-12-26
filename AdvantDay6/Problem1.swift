02870a0fa477f26e1829abade6f845c6cfb7c878
Roshanboy4u <roshankumar350@gmail.com>
2020-12-26 09:59:44 +0530

    Solution of part 1 for day 6

---
+++ AdvantDay6/Problem1.swift
@@ -0,0 +1,41 @@
+//
+//  Problem1.swift
+//  AdvantDay6
+//
+//  Created by Roshan sah on 26/12/20.
+//
+
+import Foundation
+
+struct Problem {
+    func solvePartOne(having dataSource: [String]) -> Int {
+        
+        var summationOfCount = 0
+        
+        dataSource.forEach { component in
+            let componentCount = getHashTableKeys(forString: component)
+            summationOfCount += componentCount
+        }
+        
+        return summationOfCount
+        
+    }
+    
+    func getHashTableKeys(forString input: String) -> Int {
+        let flatString = input.replacingOccurrences(of: "\n", with: "")
+        
+        var hashTable = [Character: Int]()
+        
+        for eachChar in flatString {
+            if let elementCount = hashTable[eachChar] {
+                hashTable[eachChar]! += elementCount
+            } else {
+                hashTable[eachChar] = 1
+            }
+        }
+        
+        return hashTable.keys.count
+        
+        
+    }
+}
---
+++ AdvantDay6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdvantDay6
+//
+//  Created by Roshan sah on 26/12/20.
+//
+
+import Foundation
+
+let processedArray = dataSource.components(separatedBy: "\n\n")
+
+print(Problem().solvePartOne(having: processedArray))
